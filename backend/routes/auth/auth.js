8bd2d69e64908390d38a2c60a77b07cec1d548a3
pprahas <p.prahas@protonmail.com>
2024-02-27 15:33:48 -0600

    register cleaned up

--- backend/routes/auth/auth.js
+++ backend/routes/auth/auth.js
@@ -1,5 +1,5 @@
 const User = require("../../models/UserModel");
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 const express = require("express");
 
 const router = express.Router();
@@ -9,24 +9,31 @@ router.post("/register", async (req, res) => {
   const user = req.body;
 
   try {
+    const emailCheck = await User.findOne({ email: user.email });
+    if (emailCheck) {
+      return res.status(400).json({ msg: "Email has already been taken." });
+    }
+
+    hashedPassword = await bcrypt.hash(req.body.password, 10);
+
     const dbUser = new User({
       firstName: user.firstName,
       lastName: user.lastName,
       email: user.email,
-      password: user.password,
+      password: hashedPassword,
     });
     await dbUser.save();
-    return res.status(200).json({ msg: "Your account has been created" });
+    return res.status(200).json({ msg: "Account creation success." });
   } catch (error) {
-    return res.status(400).statusMessage("wronggg");
+    return res.status(400).statusMessage("Account creation failed.");
   }
 });
 
 router.post("/login", async (req, res) => {
   const user = req.body;
-  
+
   try {
-    const email = await User.findOne({ email: user.email});
+    const email = await User.findOne({ email: user.email });
     return res.status(200).json(email);
   } catch (error) {
     return res.status(400).statusMessage("wronggg");
