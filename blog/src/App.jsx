f2b7b580a7a58012b0aebf26d8f1bab03b084530
Varshini <varshinisurapaneni@yahoo.com>
2024-02-28 11:32:08 -0600

    Home-page formatting done

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,12 +1,14 @@
 import Login from "./pages/auth/Login";
 import "./App.css";
 import Register from "./pages/auth/Register";
+import Home from "./pages/blog/Home";
 
 function App() {
   return (
     <div className="App">
       {/* <Login /> */}
-      <Register />
+      {/* <Register /> */}
+      <Home />
     </div>
   );
 }
