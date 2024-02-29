b093a9553b572e6bfd631fbfbe22141eb4a350df
pprahas <p.prahas@protonmail.com>
2024-02-29 09:53:27 -0600

    create blogs

--- blog/src/pages/blog/CreateBlog.jsx
+++ blog/src/pages/blog/CreateBlog.jsx
@@ -1,7 +1,36 @@
 import InvalidLogin from "../invalidLogin/InvalidLogin";
+import { useForm } from "../../hooks/useForm";
+import axios from "axios";
 import NavBar from "./NavBar";
 
 export default function () {
+  const [values, handleChange] = useForm({ content: "", title: "" });
+
+  let userData = window.localStorage.getItem("userData");
+  userData = JSON.parse(userData);
+  let userId = userData._id;
+  let name = userData.firstName + " " + userData.lastName;
+
+  const blogData = {
+    blog: {
+      content: values.content,
+      title: values.title,
+      name: name,
+    },
+    ID: userId, // Assuming you want to use the userId from localStorage
+  };
+
+  const handleSubmit = async (e) => {
+    axios
+      .post("http://localhost:8080/blog/create", blogData)
+      .then((res) => {
+        alert("Blog has been created!");
+      })
+      .catch((err) => {
+        alert("Blog has not  been created!");
+      });
+  };
+
   const loggedIn = window.localStorage.getItem("isLoggedIn");
   return loggedIn ? (
     <>
@@ -20,21 +49,30 @@ export default function () {
           spellCheck="false"
           placeholder="Title"
           type="text"
+          name="title"
+          value={values.title}
+          onChange={handleChange}
         />
         <textarea
           className="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none"
           spellCheck="false"
           placeholder="Describe everything about this post here"
           defaultValue={""}
+          name="content"
+          value={values.content}
+          onChange={handleChange}
         />
         <br />
         <div className="buttons flex">
           <div className="btn border border-gray-300 p-1 px-4 font-semibold cursor-pointer text-gray-500 ml-auto">
             Cancel
           </div>
-          <div className="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer text-gray-200 ml-2 bg-indigo-500">
+          <a
+            onClick={handleSubmit}
+            className="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer text-gray-200 ml-2 bg-indigo-500"
+          >
             Post
-          </div>
+          </a>
         </div>
       </div>
     </>
